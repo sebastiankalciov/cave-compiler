9e0fc52d35abb5b90a41074dfc72897998383e4d
sebastiankalciov <sebastian.kalciov@gmail.com>
2024-02-21 23:59:45 +0200

    Add basic functionality for compiler

--- cavecompiler/compiler/views.py
+++ cavecompiler/compiler/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
-# Create your views here.
+import subprocess
+
 def home(request):
-    return HttpResponse("Hello there")
+    
+    temp_cpp_file = "temp.cpp"
+
+    with open(temp_cpp_file, "w") as temp_file:
+        temp_file.write("#include <iostream> \n using namespace std; int main() {return 69;}")
+
+    run_output = "No result"
+    
+    try:
+        compile_output = subprocess.run(["g++", "-o", "temp", temp_cpp_file], capture_output=True)
+
+        if compile_output.returncode == 0:
+            run_output = subprocess.run(["./temp"], capture_output = True, text = True)
+    
+        run_output = run_output.returncode
+
+    except:
+        run_output = compile_output.stderr
+
+    page = loader.get_template("compiler/index.html")
+    return HttpResponse(run_output)
